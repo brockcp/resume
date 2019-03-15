250d5a2e9e1af4143a873d9c0af700169a773303
brockcp <brockpatterson@gmail.com>
2019-03-14 19:58:42 -0700

    Loading V3

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UiModule
+    //FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/ui/intro/intro.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss']
+})
+export class IntroComponent {
+
+ ngOnInit(){
+  $(document).ready(function(){
+    $('<img/>').attr('src','../../../assets/img/sam.jpg').on('load', function() {
+      $(this).remove();  //clean
+      $('#hero-image').css('background-image', 'url(../../../assets/img/sam.jpg');
+      $('#hero-image').addClass('add-fade');
+    });
+  });
+ }
+ 
+}
