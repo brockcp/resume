88a69558b0d535515ff2d70dc8a36fdd2ea7c24d
brockcp <brockpatterson@gmail.com>
2019-03-18 09:08:01 -0700

    adding rel paths to dynamic image

--- src/app/ui/intro/intro.component.ts
+++ src/app/ui/intro/intro.component.ts
@@ -10,9 +10,9 @@ export class IntroComponent {
 
  ngOnInit(){
   $(document).ready(function(){
-    $('<img/>').attr('src','../assets/img/sam.jpg').on('load', function() {
+    $('<img/>').attr('src','/assets/img/sam.jpg').on('load', function() {
       $(this).remove();  //clean
-      $('#hero-image').css('background-image', 'url(../assets/img/sam.jpg');
+      $('#hero-image').css('background-image', 'url(/assets/img/sam.jpg');
       $('#hero-image').addClass('add-fade');
     });
   });
